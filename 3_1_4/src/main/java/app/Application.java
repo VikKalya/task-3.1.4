4b2a70c9220d753c1c76aa7949cd853a6619761a
VikKalya <79197340+VikKalya@users.noreply.github.com>
2021-05-25 19:35:51 +0300

    Add files via upload

---
+++ 3_1_4/src/main/java/app/Application.java
@@ -0,0 +1,72 @@
+package app;
+
+import app.model.User;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class Application {
+
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        final String uri = "http://91.241.64.178:7081/api/users";
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntityGet = restTemplate.getForEntity(
+                uri,
+                String.class);
+
+        System.out.println(responseEntityGet.getBody());
+
+        StringBuilder sb = new StringBuilder();
+
+        String cookies = (responseEntityGet.getHeaders().get("Set-Cookie").get(0));
+        String[] cookie = cookies.split(";");
+
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Cookie", cookie[0]);
+
+
+        HttpEntity<User> httpEntity1 = new HttpEntity<>(new User(3, "James", "Brown", 20), httpHeaders);
+
+        ResponseEntity<String> responseEntityPost = restTemplate.exchange(
+                uri,
+                HttpMethod.POST,
+                httpEntity1,
+                String.class
+        );
+
+        sb.append(responseEntityPost.getBody());
+
+        ResponseEntity<String> responseEntityPut = restTemplate.exchange(
+                uri,
+                HttpMethod.PUT,
+                new HttpEntity<>(new User(3, "Thomas", "Shelby", 20), httpHeaders),
+                String.class
+        );
+
+        sb.append(responseEntityPut.getBody());
+
+        ResponseEntity<String> responseEntityDelete = restTemplate.exchange(
+                uri + "/3",
+                HttpMethod.DELETE,
+                new HttpEntity<>(httpHeaders),
+                String.class
+        );
+
+        sb.append(responseEntityDelete.getBody());
+
+        System.out.println(sb);
+
+    }
+}
+
+
